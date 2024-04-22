8c9aaa666154c154f2599e9b14f075048efa0ee1
edy.ismanto <edy.ismanto@psn.co.id>
2024-04-22 17:03:38 +0700

    remove: unnecessary package

--- config/settings.py
+++ config/settings.py
@@ -8,18 +8,14 @@ env_path = find_dotenv()
 config = dotenv_values(env_path)
 
 class Config:
-    def __init__(self, cfg):
-        self.config = cfg
-        
+    
     DB_URL: str = config['DB_URL']
     DB_MODELS: List[str] = ['models.user']
-    SECRET: str='ce98bfa084c9afe1b2b121e7c028607e0e5883878f08f57fa67f0f667b39c6e0'
+    SECRET: str='1cb4debd82fcf0c6f31d1cccfce4f1c9117a0cad21b71d5c2eab1c050e878701'
     ALGORITHM: str='HS256'
     JWT_ACCESS_EXP: timedelta = timedelta(days=1)
     JWT_REFRESH_EXP: timedelta = timedelta(days=365)
     
-    # def connectMongo(self):
-    #     return MongoClient(self.config['URLMONGO'])
     MONGO_URL: str = config['URLMONGO']
     
     
