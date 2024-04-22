eb8709dd1c1f95c22652afd91cfbb1b8befdc2eb
edy.ismanto <edy.ismanto@psn.co.id>
2024-04-22 16:34:53 +0700

    remove: unnecessary package

--- config/database.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector as mysql
-from pymongo import MongoClient
-
-class Config:
-    def __init__(self, cfg):
-        self.config = cfg
-        
-    def connectMongo(self):
-        return MongoClient(self.config['URLMONGO'])
